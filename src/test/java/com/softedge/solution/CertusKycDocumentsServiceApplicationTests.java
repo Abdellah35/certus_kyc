11b8ab57b47a0a4f3e22711e077db4e9a34252fb
Abdellah Kamil <abdellah.kamil2017@gmail.com>
2024-02-05 16:35:06 +0300

    Update CertusKycDocumentsServiceApplicationTests.java

--- src/test/java/com/softedge/solution/CertusKycDocumentsServiceApplicationTests.java
+++ src/test/java/com/softedge/solution/CertusKycDocumentsServiceApplicationTests.java
@@ -3,10 +3,10 @@ package com.softedge.solution;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class CertusKycDocumentsServiceApplicationTests {
 
-	//@Test
+	@Test
 	void contextLoads() {
 	}
 
